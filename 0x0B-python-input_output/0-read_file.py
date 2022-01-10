eb437576fe6c9f30af0edbc8cb34dc0beacd3071
uwitonzebrendah <brendahuwitonze1@gmail.com>
2022-01-11 00:46:30 +0200

    0-red

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-'''A module containing IO functions.
-'''
+"""
+Module for function that reads a text file
+"""
 
 
 def read_file(filename=""):
-    '''Reads a UTF-8 encoded text file and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to read.
-    '''
-    with open(filename, encoding='utf-8') as file:
-        for line in file.readlines():
-            print(line, end='')
+    """ function that reads UTF8 file and prints to stdout """
+    with open(filename, encoding='utf-8') as a_file:
+        for line in a_file:
+            print(line, end="")
